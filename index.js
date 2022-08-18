81b8ced1fceb80aff4d0e6c58ee07f292026f75d
MercyChebet <chebetm96@gmail.com>
2022-08-18 12:04:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
+  
+  if (ride <= 400){
+return 'This one is on me!';
+  }
+  else if(ride > 2000 && ride < 2500){
 
-function ternaryCheckCity(){
-  // Write your code here!
+    return 'I will gladly take your thirty bucks.';
+  }
+   else if(ride > 2500){
+  return 'No can do.';
 }
+}
+function ternaryCheckCity(city){
+  let result = (city === 'NYC')? 'Ok, sounds good.' : 'No go.';
+  return result 
+}
+ternaryCheckCity('NYC')
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+    return 'Thank you so much.';
+    break;
+    case 'not as generous':
+    return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
